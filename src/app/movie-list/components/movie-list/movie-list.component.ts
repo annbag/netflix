ab80b8f44b26a6e849582ffd4b1ddf7d34ddc66a
Anna Ambroziak <ambroziak_anna@wp.pl>
2020-06-28 18:38:49 +0200

    change detection strategy onPush

--- src/app/movie-list/components/movie-list/movie-list.component.ts
+++ src/app/movie-list/components/movie-list/movie-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { MovieListService } from '../../services/movie-list.service';
 import { MovieList } from '../../interfaces/movie-list.interface';
 
 @Component({
     selector: 'app-movie-list',
     templateUrl: './movie-list.component.html',
-    styleUrls: ['./movie-list.component.scss']
+    styleUrls: ['./movie-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieListComponent implements OnInit {
     movies: MovieList = [];
 
-    constructor(private movieListService: MovieListService) { }
+    constructor(private movieListService: MovieListService,
+        private cdr: ChangeDetectorRef) { }
 
     ngOnInit() {
         this.loadMovieList();
@@ -20,6 +22,7 @@ export class MovieListComponent implements OnInit {
         this.movieListService.getMovieList().subscribe({
             next: (movies) => {
                 this.movies = movies;
+                this.cdr.detectChanges();
             }
         });
     }
--- src/app/movie-list/components/movie/movie.component.ts
+++ src/app/movie-list/components/movie/movie.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ChangeDetectionStrategy } from '@angular/core';
 import { Movie } from '../../interfaces/movie-list.interface';
 
 @Component({
     selector: 'app-movie',
     templateUrl: './movie.component.html',
-    styleUrls: ['./movie.component.scss']
+    styleUrls: ['./movie.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieComponent implements OnInit {
+
+    public isSelected: boolean;
+
+    // @HostListener('mouseover') mouseoverHandler() {
+        // this.isSelected = true;
+    // }
+
+    // @HostListener('mouseout') mouseoutHandler() {
+        // this.isSelected = false;
+    // }
 
     @Input() movie: Movie = null;
     constructor() { }
 
     ngOnInit() {
+    }
+
+    public displayInfoAboutPrice() {
+        console.log('displayInfoAboutPrice')
+        return this.movie.title.substr(-1) === 'a' ? 'darmowy' : 'płatny'
     }
 }
